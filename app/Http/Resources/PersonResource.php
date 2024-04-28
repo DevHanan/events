7ad0b2aba2c735dfa0a40936f7e0b8f92ff3de15
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2024-04-28 12:40:55 +0300

    add person module

--- app/Http/Resources/PersonResource.php
+++ app/Http/Resources/PersonResource.php
@@ -18,6 +18,27 @@ class PersonResource extends JsonResource
             "id"=> $this->id,
             "first_name"=> $this->first_name,
             "nationality"=>$this->country->nationality ?? '',
+            'surName' => $this->surName, 
+            'name_tag' => $this->name_tag,
+            'ID'  => $this->ID,
+            'id_type'=>  $this->id_type,
+            'id_expiration' => $this->id_expiration,
+            'Honor' => $this->Honor,
+            'email' => $this->email, 
+            'birthdate' => $this->birthdate,
+            'gender'  => $this->gender,
+            'zip'=>  $this->zip,
+
+            'Phone1' => $this->Phone1, 
+            'WhatsApp' => $this->WhatsApp,
+            'street_address'  => $this->street_address,
+            'linkedIn'=>  $this->linkedIn,
+            'Phone1' => $this->Job_title, 
+            'WhatsApp' => $this->city_id,
+            'street_address'  => $this->country_id,
+            'linkedIn'=>  $this->leader_Sn,
+            'account_number'=>  $this->account_number
+
 
 
         ];
